a2aae9688d87513baa611aab87dc6cf621f976ec
Ankitaa1129 <133750909+Ankitaa1129@users.noreply.github.com>
2024-02-06 11:06:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+                        const slider = document.querySelector('.slideshow');
+          const slides = slider.querySelectorAll('li');
+          
+// Store the total number of images
+         const slideCount = slides.length;
+        let activeSlide = 0;
+
+// To change the images dynamically every
+// 5 Secs, use SetInterval() method
+setInterval(() => {
+	      slides[activeSlide].classList.remove('active');
+	       activeSlide++;
+	      if (activeSlide === slideCount) {
+	      	activeSlide = 0;
+	      }
+	      slides[activeSlide].classList.add('active');
+       }, 2000);
+
+        
